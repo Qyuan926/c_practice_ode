dbb8a4ff60363e03d45f3b2fd58e99bc01630a14
Qyuan926 <lqy6323@foxmail.com>
2019-11-10 20:27:49 +0800

    Remove Element
    Given an array nums and a value val, remove all instances of that value in-place and return the new length.
    
    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
    
    The order of elements can be changed. It doesn't matter what you leave beyond the new length.

---
+++ Remove Element/Remove Element/Remove Element.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+/*使用两个指针start,tail，如果一个指针start指向的 是查找的数字地址start往后移一个另一个指针tail不动，如果不是，用*start代替*tail/
+int removeElement(int* nums, int numsSize, int val){
+	int *head = nums;
+	int *start = nums;
+	int *tail = nums;
+	while (start<head + numsSize)
+	{
+		if (*start == val)
+		{
+			start++;
+		}
+		else
+		{
+			*tail = *start;
+			start++;
+			tail++;
+		}
+
+	}
+	return tail - head;
+
+}
+int main()
+{
+	int nums[5] = { 3,2,3,5,3 };
+	int val = 3;
+	int n=removeElement(nums, 5, val);
+	printf("%d", n);
+	return 0;
+
+}
